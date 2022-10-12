fb9748d2731d658ccc6d1309da9c009a5619bcc7
Mike Cronce <mike@quadra-tec.net>
2022-10-12 17:26:14 -0400

    src/v2/manifest/mod.rs:  derive Deserialize and Serialize for Manifest

--- src/v2/manifest/mod.rs
+++ src/v2/manifest/mod.rs
@@ -274,7 +274,7 @@ fn build_accept_headers(accepted_types: &[(MediaTypes, Option<f64>)]) -> header:
 }
 
 /// Umbrella type for common actions on the different manifest schema types
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub enum Manifest {
     S1Signed(manifest_schema1::ManifestSchema1Signed),
     S2(manifest_schema2::ManifestSchema2),
