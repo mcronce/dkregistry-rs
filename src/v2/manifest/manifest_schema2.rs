0c106d6011912d8835be8245a7c3aa0492f1e10e
Mike Cronce <mike@quadra-tec.net>
2023-06-29 11:22:51 -0400

    src/v2/manifest/manifest_schema2.rs:  Fix error compiling

--- src/v2/manifest/manifest_schema2.rs
+++ src/v2/manifest/manifest_schema2.rs
@@ -5,7 +5,7 @@ use reqwest::Method;
 /// Manifest version 2 schema 2.
 ///
 /// Specification is at <https://docs.docker.com/registry/spec/manifest-v2-2/>.
-#[derive(Debug, Default, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ManifestSchema2Spec {
     #[serde(rename = "schemaVersion")]
     schema_version: u16,
